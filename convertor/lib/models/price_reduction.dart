ae902a2a9e4e6025ada9457a7346296d38600c89
Benjamin <benjamin.dembin@gmail.com>
2021-01-12 14:35:37 +0100

    fichier calcul réduction prix

---
+++ convertor/lib/models/price_reduction.dart
@@ -0,0 +1,22 @@
+class PriceReduction{
+
+    double price; // prix de départ
+    int reduction; // valeur de la réduction
+
+    PriceReduction(double myPrice, int myReduction){
+        
+      this.price = myPrice;
+      this.reduction = myReduction;
+    }
+
+    Map<String, Object> Calcul(double myPrice, int myReduction){
+      
+      double thisReduc = (myPrice * myReduction)/100; // calcule de l'economie
+      double endPrice= myPrice - thisReduc; // prix à payer 
+      return {
+        'endPrice': endPrice,
+        'reduc': thisReduc
+      }; // renvoie le  prix à payer et  la valeur de l'économie sous format Map<String, object>
+    }
+ 
+}
