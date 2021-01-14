249c8dab3bb32eda6d94f51f094a8426b4ee96ca
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2021-01-14 10:39:38 +0100

    Feat: correct coef

--- convertor/lib/models/area.dart
+++ convertor/lib/models/area.dart
@@ -1,29 +1,31 @@
 class Area {
-  int area;
+  double area;
 
   String unit;
 
   Area(this.area, this.unit);
 
   Map<String, Object> toMeterAreaCoefficients = {
-    'cm²': 10000,
-    'm²': 1,
-    'km²': 1 / 1000000,
-    'ha': 1 / 10000,
-    'ac': 1 / 4047,
+    'cm²': 1,
+    'm²': 10000,
+    'km²': 10000000000,
+    'ha': 100000000,
+    'ac': 40470000,
   };
 
   Map<String, Object> fromMeterAreaToOtherCoefficients = {
-    'cm²': 10000,
-    'm²': 1,
-    'km²': 1 / 1000000,
-    'ha': 1 / 10000,
-    'ac': 1 / 4047,
+    'cm²': 1,
+    'm²': 1 / 10000,
+    'km²': 1 / 10000000000,
+    'ha': 1 / 100000000,
+    'ac': 1 / 40470000,
   };
 
-  double convertArea(String unitToConvertTo){
-    double sizeToMeter = (this.area * toMeterAreaCoefficients[this.unit]).toDouble();
-    double sizeToUnit = sizeToMeter * fromMeterAreaToOtherCoefficients[unitToConvertTo];
+  double convertArea(String unitToConvertTo) {
+    double sizeToMeter =
+        (this.area * toMeterAreaCoefficients[this.unit]).toDouble();
+    double sizeToUnit =
+        sizeToMeter * fromMeterAreaToOtherCoefficients[unitToConvertTo];
 
     return sizeToUnit;
   }
