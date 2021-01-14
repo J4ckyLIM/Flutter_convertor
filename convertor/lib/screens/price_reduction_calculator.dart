2f5873cce27ceeb28cf779f54cf1742c8ec029d6
Benjamin <benjamin.dembin@gmail.com>
2021-01-14 19:22:15 +0100

    price reduction finally works

--- convertor/lib/screens/price_reduction_calculator.dart
+++ convertor/lib/screens/price_reduction_calculator.dart
@@ -1,17 +1,43 @@
+import 'dart:ffi';
+
+import 'package:convertor/models/price_reduction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PriceReductionCalculator extends StatefulWidget {
   PriceReductionCalculator({Key key, this.title}) : super(key: key);
 
   final String title;
+  
+  
 
   @override
   _PriceReductionCalculatorState createState() =>
       _PriceReductionCalculatorState();
 }
 
+
+
+
 class _PriceReductionCalculatorState extends State<PriceReductionCalculator> {
+  
+  double prixSaisis; 
+  int pourcentSaisis; 
+  double economie; 
+  double prixAPayer;
+  bool isVisible = false;
+  
+  PriceReduction myPriceReduction = PriceReduction(0.0 , 0); 
+  
+ 
+
   @override
+ void initState(){
+   super.initState();
+   myPriceReduction = new PriceReduction(prixSaisis, pourcentSaisis);
+ }
+ 
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,14 +45,96 @@ class _PriceReductionCalculatorState extends State<PriceReductionCalculator> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child : Text( 'Prix :'),
+                  
+                ),
+                Container(
+                  width: 300,
+                  child: TextField(
+                    onSubmitted: (String monPrixSaisis){
+                     if( double.tryParse(monPrixSaisis) != null ){
+
+                       setState(() {
+                        prixSaisis= double.parse(monPrixSaisis);
+                        }
+                      );
+
+                     }
+                      
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Saisir le prix de l'article",
+                      ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),    
+          
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container( 
+                  child: Text( 'Réduction :'),
+                ),
+                Container(
+                  width: 300,
+                  child: TextField(
+                    onSubmitted: (String maReduction){
+                      if(int.tryParse(maReduction) != null){
+                        setState(() {
+                          pourcentSaisis = int.parse(maReduction);
+                        });
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Saisir la réduction",
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                )
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: IconButton(icon: Icon(Icons.check_box),
+                    iconSize: 50,
+                    onPressed: (){
+                      List reduction = PriceReduction.calcul(prixSaisis, 
+                      pourcentSaisis); 
+                      setState(() {
+                        isVisible = true;
+                        economie = reduction.last;
+                        prixAPayer = reduction.first;
+                      });
+                      
+                    },
+                  ),
+                ),
+              ],
+            ),
+            isVisible ? Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container( 
+                  child: Text('Vous économiser: $economie € il vous reste à payer $prixAPayer €'),
+                ),
+              ],
+            ) : Container(),
+            
           ],
         ),
+
       ),
     );
   }
 }
+           
