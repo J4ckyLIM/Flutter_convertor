4efba5845a73d4e87be8b2e03ce6af581e90ff54
Micael <micael.dossantos@edu.itescia.fr>
2021-01-14 22:17:01 +0100

    Fix class roman and begin of front roman-convertor

--- convertor/lib/screens/roman_convertor.dart
+++ convertor/lib/screens/roman_convertor.dart
@@ -1,3 +1,4 @@
+import 'package:convertor/models/roman.dart';
 import 'package:flutter/material.dart';
 
 class RomanConvertor extends StatefulWidget {
@@ -10,6 +11,11 @@ class RomanConvertor extends StatefulWidget {
 }
 
 class _RomanConvertorState extends State<RomanConvertor> {
+
+  //Roman _roman2 = Roman(5, "V");
+
+  Roman _roman1 = Roman(1 , "I");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +24,67 @@ class _RomanConvertorState extends State<RomanConvertor> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Icon(Icons.expand),
+                ),
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 300,
+                      child:
+                      TextFormField(
+                        key: Key(_roman1.value.toString()),
+                        initialValue: _roman1.value.toString(),
+                        keyboardType: TextInputType.number,
+                        onChanged: (String value) {
+                          setState(() {
+                            _roman1.value = int.parse(value);
+
+                            _roman1.romanValue = _roman1.convertToRoman().toString();
+                          });
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Value to convert'),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 300,
+                      child:
+                      TextFormField(
+                        key: Key(_roman1.romanValue),
+                        initialValue: _roman1.romanValue,
+                        onChanged: (String value) {
+                          setState(() {
+                            _roman1.romanValue = value;
+
+                            _roman1.value = _roman1.convertRomanToInt(_roman1.romanValue);
+                          });
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Value to convert'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
