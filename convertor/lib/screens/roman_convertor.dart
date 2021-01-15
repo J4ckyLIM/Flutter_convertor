90eea346188ed000d5beab21aef46cec9dc4118f
Micael <micael.dossantos@edu.itescia.fr>
2021-01-15 10:12:55 +0100

    Front of roman-convertor

--- convertor/lib/screens/roman_convertor.dart
+++ convertor/lib/screens/roman_convertor.dart
@@ -11,6 +11,7 @@ class RomanConvertor extends StatefulWidget {
 }
 
 class _RomanConvertorState extends State<RomanConvertor> {
+  final MaterialColor mainThemeColor = Colors.teal;
 
   Roman _roman1 = Roman(1 , "I");
 
@@ -27,61 +28,76 @@ class _RomanConvertorState extends State<RomanConvertor> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  child: Icon(Icons.expand),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Image(
+                      height: 250,
+                      image: AssetImage(
+                          'assets/roman-image.jpg'
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 300,
-                      child:
-                      TextFormField(
-                        key: Key(_roman1.value.toString()),
-                        initialValue: _roman1.value.toString(),
-                        keyboardType: TextInputType.number,
-                        onChanged: (String value) {
-                          setState(() {
-                            _roman1.value = int.parse(value);
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: new EdgeInsets.symmetric(horizontal: 20.0),
+                        width: 300,
+                        child:
+                        TextFormField(
+                          key: Key(_roman1.value.toString()),
+                          initialValue: _roman1.value.toString(),
+                          keyboardType: TextInputType.number,
+                          onChanged: (String value) {
+                            setState(() {
+                              _roman1.value = int.parse(value);
 
-                            _roman1.romanValue = _roman1.convertToRoman().toString();
-                          });
-                        },
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Value to convert'),
+                              _roman1.romanValue = _roman1.convertToRoman().toString();
+                            });
+                          },
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Decimal',
+                              hintText: 'Value to convert'),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 300,
-                      child:
-                      TextFormField(
-                        key: Key(_roman1.romanValue),
-                        initialValue: _roman1.romanValue,
-                        onChanged: (String value) {
-                          setState(() {
-                            _roman1.romanValue = value;
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: new EdgeInsets.symmetric(horizontal: 20.0),
+                        width: 300,
+                        child:
+                        TextFormField(
+                          key: Key(_roman1.romanValue),
+                          initialValue: _roman1.romanValue,
+                          onChanged: (String value) {
+                            setState(() {
+                              _roman1.romanValue = value;
 
-                            _roman1.value = _roman1.convertRomanToInt(_roman1.romanValue);
-                          });
-                        },
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Value to convert'),
+                              _roman1.value = _roman1.convertRomanToInt(_roman1.romanValue);
+                            });
+                          },
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Roman numeral',
+                              hintText: 'Value to convert'),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
